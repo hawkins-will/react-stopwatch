3b87616d9926e38ada9f626cde4290326b2a129f
Will Hawkins <willhawkins123@yahoo.com>
2017-03-24 15:52:20 -0400

    Stop Watch Completeed React is Making More Sense

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor(props){
+    super()
+    this.state = {
+      counter: 0
+    }
+  }
+  start() {
+    this.timer = setInterval(()=> {
+      this.state.counter++
+      this.setState({counter: this.state.counter})}, 1000)
+    }
+  pause() {
+    clearInterval(this.timer)
+  }
+  reset() {
+    clearInterval(this.timer)
+    this.setState({counter: 0})
+  }
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.counter}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={() => this.reset()}>Reset</button>
+          <button onClick={() => this.start()}>Start</button>
+          <button onClick={() => this.pause()}>Pause</button>
         </div>
       </div>
     );
